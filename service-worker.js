b44bc649199ad89940a37a0d4fb59c28b0deda8f
Lozairio <theophilustietie@gmail.com>
2025-07-26 15:09:02 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,18 @@
-const CACHE_NAME = 'bet-value-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon.png'
-];
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open('value-bet-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json'
+      ]);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
